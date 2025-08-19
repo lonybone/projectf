d1f50f9f28b5d8f71979adc24eda2909a41107f3
lonybone <go68let@mytum.de>
2025-08-19 02:24:38 +0200

    added dynamicArray .c and .h for handling specificly a dynamic array of Statement pointers which will be used for implementing the AST. more specifically for the list of stement pointers directly under the root

---
+++ source/dynamicArray.h
@@ -0,0 +1,20 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+#include "parser.h"
+
+typedef struct DynamicArray DynamicArray;
+static const int INITIAL_CAPACITY = 16;
+
+struct DynamicArray {
+	int growthFactor;
+	int maxSize;
+	int size;
+	Statement** array;
+};
+
+DynamicArray* dynamicArray(int growthFactor);
+Statement* get(DynamicArray* dynamicArray, int idx);
+int append(DynamicArray* dynamicArray, Statement* statement);
+void freeArray(DynamicArray* dynamicArray);
+
+#endif
